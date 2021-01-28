15f5dea92f1c0652fd875923964a7939c7288694
unknown <parist.sado@bumail.net>
2021-01-28 20:00:53 +0700

    Send Assignmenr Week1

---
+++ GI455_Week1/Assets/ModTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModTest : MonoBehaviour
+{
+    public InputField ipField;
+    public Text Show;
+    public GameObject text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Search()
+    {
+        Hashtable perD = new Hashtable();
+        perD.Add("Happy", "Happy");
+        perD.Add("Run", "Run");
+        perD.Add("Shoot", "Shoot");
+        perD.Add("Walk", "Walk");
+        foreach (string word in perD.Keys)
+        {
+            if (text = GameObject.Find(ipField.text))
+            {
+
+                Show.text = ipField.text + " found.";
+
+            }
+            else Show.text = ipField.text + " not found.";
+
+        }
+    }
+}
---
+++ GI455_Week1/Assets/SearchBT.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchBT : MonoBehaviour
+{
+    public InputField ipField;
+    public Text Show;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Search()
+    {
+        List<string> list = new List<string>() { "Happy", "Walk", "Shoot", "Run","Jump" };
+        foreach (string word in list)
+        {
+            if (text = GameObject.Find(ipField.text))
+            {
+           
+                Show.text = ipField.text + " found.";
+                
+            }else Show.text = ipField.text + " not found.";
+               
+        }
+    }
+}
